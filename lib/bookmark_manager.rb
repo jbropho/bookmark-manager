476935a58cfe480dabf80d04b152fe6093e8190b
Jordan Brooks <jordanbrooks@MacBook-Pro-68.local>
2018-04-10 14:36:07 +0100

    refactor bookmark manager

---
+++ lib/bookmark_manager.rb
@@ -0,0 +1,32 @@
+require_relative'helper'
+
+class BookmarkManager
+   def self.all
+     execute 'SELECT url FROM bookmarks'
+   end
+
+   def self.add(url)
+     raise "#{url} is not a valid url" unless self.valid_url?(url)
+     execute('INSERT INTO bookmarks (url) VALUES ($1)', [url])
+   end
+
+   private
+   def self.execute(request, params = [])
+     connect
+     result = @conn.exec(request, params)
+     close
+     result
+   end
+
+   def self.connect
+     @conn = PG::Connection.open(Database::database_connection)
+   end
+
+   def self.close
+     @conn.close
+   end
+
+   def self.valid_url?(url)
+    !!(url.match(/https*\:\/\/[\w\.\-]{5,}\.[a-z]+/))
+   end
+end
