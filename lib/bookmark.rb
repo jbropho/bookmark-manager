d007c5797917af75001e303bd1e97bef4a1d6b44
Jordan Brooks <jordanbrooks@MacBook-Pro-68.local>
2018-04-10 14:15:36 +0100

    refactor Bookmark clas

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,17 +1,33 @@
-require 'pg'
 require 'pry'
 require_relative'helper'
 
 class Bookmark
    def self.all
-     conn = PG::Connection.open(Database::database_connection)
-     res = conn.exec_params('SELECT url FROM bookmarks')
-     res
+     execute 'SELECT url FROM bookmarks'
    end
 
    def self.add(url)
-     conn = PG::Connection.open(Database::database_connection)
-     res = conn.exec_params('INSERT INTO bookmarks (url) VALUES ($1)', [url])
-     res
+     raise "#{url} is not a valid url" unless self.valid_url?(url)
+     execute('INSERT INTO bookmarks (url) VALUES ($1)', [url])
+   end
+
+   private
+   def self.execute(request)
+     connect
+     result = @conn.exec(request)
+     close
+     result
+   end
+
+   def self.connect
+     @conn = PG::Connection.open(Database::database_connection)
+   end
+
+   def self.close
+     @conn.close
+   end
+
+   def self.valid_url?(url)
+    !!(url.match(/https*\:\/\/[\w\.\-]{5,}\.[a-z]+/))
    end
 end
--- lib/helper.rb
+++ lib/helper.rb
@@ -1,11 +1,12 @@
 module Database
+  require 'pg'
   def self.database_connection
     ENV['RACK_ENV'] == 'test' ?
     {:dbname => 'bookmark_manager_test'} : {:dbname => 'bookmark_manager'}
   end
 
   def self.add_test_data
-    conn = PG::Connection.open(:dbname => 'bookmark_manager_test')
+    conn = PG::Connection.open(self.database_connection)
     conn.exec_params("INSERT INTO bookmarks (url) VALUES ('http://www.github.com')")
   end
 end
